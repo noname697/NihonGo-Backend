f7a510965503265ea3b21784158fa36e0e4d64c3
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-17 21:32:08 -0300

    feat: Criando o servidor express

---
+++ src/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const app = require("./app.js");
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`NihonGo! API running on port ${PORT}`);
+});
