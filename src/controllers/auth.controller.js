dde55372f3e32f3b57f6d8706db14a657d14d455
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-20 18:08:00 -0300

    feat: Criando controller de autenticação

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+const authService = require("../services/auth.service");
+
+const register = async (req, res, next) => {
+  try {
+    const result = await authService.registerUser(req.body);
+
+    return res.status(201).json({
+      message: "User created successfully.",
+      ...result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const login = async (req, res, next) => {
+  try {
+    const result = await authService.loginUser(req.body);
+
+    return res.status(200).json({
+      message: "Login successfully.",
+      ...result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  register,
+  login,
+};
