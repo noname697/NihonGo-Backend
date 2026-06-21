271315063ad7b80b9846d7368dc82a0bd4b17c61
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-21 15:49:33 -0300

    Atualizando rotas para o conteúdo

---
+++ src/routes/content.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const contentController = require("../controllers/content.controller");
+
+const router = express.Router();
+
+router.get("/modules", contentController.listModules);
+router.get("/modules/:id", contentController.showModule);
+router.get("/lessons/:id", contentController.showLesson);
+router.get("/lessons/:id/exercises", contentController.listLessonExercises);
