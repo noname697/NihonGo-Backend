5e3d00f1a0a70deaa2e16b151960177d98222688
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-24 13:47:40 -0300

    feat: Ajustando models do progresso

--- models/userexerciseprogress.js
+++ models/userexerciseprogress.js
@@ -1,28 +1,52 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class UserExerciseProgress extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      UserExerciseProgress.belongsTo(models.User, {
+        foreignKey: "user_id",
+        as: "user",
+      });
+      UserExerciseProgress.belongsTo(models.Exercise, {
+        foreignKey: "exercise_id",
+        as: "exercise",
+      });
     }
   }
-  UserExerciseProgress.init({
-    user_id: DataTypes.INTEGER,
-    exercise_id: DataTypes.INTEGER,
-    answer: DataTypes.STRING,
-    is_correct: DataTypes.BOOLEAN,
-    attempts_count: DataTypes.INTEGER,
-    answered_at: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'UserExerciseProgress',
-  });
+  UserExerciseProgress.init(
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      exercise_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      answer: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      is_correct: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      attempts_count: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+      },
+      answered_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "UserExerciseProgress",
+      tableName: "user_exercise_progress",
+    },
+  );
   return UserExerciseProgress;
 };
--- models/userlessonprogress.js
+++ models/userlessonprogress.js
@@ -1,30 +1,62 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class UserLessonProgress extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      UserLessonProgress.belongsTo(models.User, {
+        foreignKey: "user_id",
+        as: "user",
+      });
+      UserLessonProgress.belongsTo(models.Lesson, {
+        foreignKey: "lesson_id",
+        as: "lesson",
+      });
     }
   }
-  UserLessonProgress.init({
-    user_id: DataTypes.INTEGER,
-    lesson_id: DataTypes.INTEGER,
-    total_exercises: DataTypes.INTEGER,
-    correct_exercises: DataTypes.INTEGER,
-    score: DataTypes.FLOAT,
-    is_completed: DataTypes.BOOLEAN,
-    completed_at: DataTypes.DATE,
-    last_studied_at: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'UserLessonProgress',
-  });
+  UserLessonProgress.init(
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      lesson_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      total_exercises: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      correct_exercises: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      score: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      is_completed: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      completed_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      last_studied_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "UserLessonProgress",
+      tableName: "user_lesson_progress",
+    },
+  );
   return UserLessonProgress;
 };
