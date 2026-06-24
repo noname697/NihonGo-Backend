0a38ef22ad3c83f514a683161def1f256a9b3133
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-06-24 13:39:54 -0300

    feat: Ajustando migrations de progresso

--- migrations/20260624005807-create-user-exercise-progress.js
+++ migrations/20260624005807-create-user-exercise-progress.js
@@ -1,43 +1,75 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('UserExerciseProgresses', {
+    await queryInterface.createTable("user_exercise_progress", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       user_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
       exercise_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "exercises",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
       answer: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       is_correct: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
       },
       attempts_count: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
       },
       answered_at: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
+
+    await queryInterface.addIndex("user_exercise_progress", ["user_id"]);
+    await queryInterface.addIndex("user_exercise_progress", ["exercise_id"]);
+
+    await queryInterface.addIndex(
+      "user_exercise_progress",
+      ["user_id", "exercise_id"],
+      {
+        unique: true,
+      },
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('UserExerciseProgresses');
-  }
+    await queryInterface.dropTable("user_exercise_progress");
+  },
 };
--- migrations/20260624010019-create-user-lesson-progress.js
+++ migrations/20260624010019-create-user-lesson-progress.js
@@ -1,49 +1,84 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('UserLessonProgresses', {
+    await queryInterface.createTable("user_lesson_progress", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       user_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
       lesson_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "lessons",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
       total_exercises: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
       },
       correct_exercises: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
       },
       score: {
-        type: Sequelize.FLOAT
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
       },
       is_completed: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
       },
       completed_at: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull: true,
       },
       last_studied_at: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull: true,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
+
+    await queryInterface.addIndex("user_lesson_progress", ["user_id"]);
+    await queryInterface.addIndex("user_lesson_progress", ["lesson_id"]);
+
+    await queryInterface.addIndex(
+      "user_lesson_progress",
+      ["user_id", "lesson_id"],
+      {
+        unique: true,
+      },
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('UserLessonProgresses');
-  }
+    await queryInterface.dropTable("user_lesson_progress");
+  },
 };
